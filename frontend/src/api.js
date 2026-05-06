40b687327b92a34588e311da3c25b6016b5ef6f0
Ishika Sharma <142723172+ishika-xrma@users.noreply.github.com>
2026-05-06 20:45:32 +0530

    Update API base URL to use environment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-export const API =
-  axios.create({
-    baseURL:
-      "http://localhost:8000"
-  });
+export const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
